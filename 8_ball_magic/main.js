f2840db4db25b9819c7682aa067e9a59fb1810c9
Shiv_Boy <shivamchouhan728@gmail.com>
2021-02-20 21:52:16 +0530

    :G: 8_ball_magic

---
+++ 8_ball_magic/main.js
@@ -0,0 +1,39 @@
+userName = "";
+
+userName ? console.log(`Hello, ${userName}`) : console.log(`Hello stranger`);
+
+const userQuestion = ("How many brain cells works at a time");
+console.log(`The user asked: ${userQuestion}`);
+
+randomNumber = Math.floor(Math.random() * 8);
+
+eightBall = "";
+
+switch (randomNumber) {
+    case 0:
+        eightBall = 'It is certain';
+        break;
+    case 1:
+        eightBall = 'It is decidedly so';
+        break;
+    case 2:
+        eightBall = 'Reply hazy try again';
+        break;
+    case 3:
+        eightBall = 'Cannot predict now';
+        break;
+    case 4:
+        eightBall = 'Do not count on it';
+        break;
+    case 5:
+        eightBall = 'My sources say no';
+        break;
+    case 6:
+        eightBall = 'Outlook not so good';
+        break;
+    case 7:
+        eightBall = 'Signs point to yes';
+        break;
+}
+
+console.log(eightBall)
