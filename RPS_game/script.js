5e32fce9ac9a2f1912535a311c30661b723890ed
shivam <shivamchouhan728@gmail.com>
2021-03-05 16:47:30 +0530

    adds playgame function with scret move

--- RPS_game/script.js
+++ RPS_game/script.js
@@ -1,6 +1,6 @@
 const getUserChoice = (userInput) => {
     userInput = userInput.toLowerCase();
-    if (userInput === 'rock' || userInput == 'paper' || userInput === 'scissorss') {
+    if (userInput === 'rock' || userInput == 'paper' || userInput === 'scissors' || userInput === 'bomb') {
         return userInput;
     } else {
         console.log("Invalid input!");
@@ -20,18 +20,33 @@ const getComputerChoice = () => {
     }
 };
 
-
 // ''' we know that r > s, s > p, p > r'''
 const determineWinner = (userChoice, computerChoice) => {
     if (userChoice === computerChoice) {
         return "The game is a Tie";
+    } else if (userChoice == 'bomb') {
+        return "Wow You got all the screts Congratulations!! on your victory!"
     } else if (userChoice === 'rock' && computerChoice === 'scissors') {
-        return "User Won the game!"
+        return "You Won the game!";
     } else if (userChoice === 'scissors' && computerChoice === 'paper') {
-        return "User Won the game!"
+        return "You Won the game!";
     } else if (userChoice === 'paper' && computerChoice === 'rock') {
-        return "User Won the game!"
+        return "You Won the game!";
     } else {
-        return "Computer Won the game!"
+        return "Computer Won the game!";
     }
 };
+
+const playGame = () => {
+    const userChoice = getUserChoice('paper');
+    const computerChoice = getComputerChoice();
+    console.log('You threw: ' + userChoice);
+    console.log('The computer threw:' + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+};
+
+
+
+playGame()
+
+
