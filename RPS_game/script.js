f82511c4d552d54050fca060ba8a1968925590c2
shivam <shivamchouhan728@gmail.com>
2021-03-05 16:19:12 +0530

    adds function determineWinner

--- RPS_game/script.js
+++ RPS_game/script.js
@@ -7,8 +7,6 @@ const getUserChoice = (userInput) => {
     }
 };
 
-// console.log(getUserChoice('paper'))
-
 
 const getComputerChoice = () => {
     randomNumber = Math.floor(Math.random() * 3);
@@ -19,5 +17,21 @@ const getComputerChoice = () => {
             return 'paper';
         case 2:
             return 'scissors'
+    }
+};
+
+
+// ''' we know that r > s, s > p, p > r'''
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+        return "The game is a Tie";
+    } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+        return "User Won the game!"
+    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+        return "User Won the game!"
+    } else if (userChoice === 'paper' && computerChoice === 'rock') {
+        return "User Won the game!"
+    } else {
+        return "Computer Won the game!"
     }
 };
