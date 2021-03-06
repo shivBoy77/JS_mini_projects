45a60dc7ce73cf372cf9b9e61d070182293e7224
shivam <shivamchouhan728@gmail.com>
2021-03-06 20:36:23 +0530

    adds idealSleep func & Calculate sleep hours

--- sleepDebtCalculator/scripts.js
+++ sleepDebtCalculator/scripts.js
@@ -1,30 +1,30 @@
 const getSleepHours = (day) => {
     switch (day) {
-        case 'monday':
+        case 'Monday':
             return 8
             break;
-        case 'tuesday':
+        case 'Tuesday':
             return 5
             break;
         case 'Wednesday':
             return 3
             break;
-        case 'thursday':
+        case 'Thursday':
             return 5
             break;
-        case 'friday':
+        case 'Friday':
             return 8
             break;
-        case 'saturday':
+        case 'Saturday':
             return 4
             break;
-        case 'sunday':
+        case 'Sunday':
             return 9
             break;
         default:
             return "Error!"
     }
-}
+};
 
 const getActualSleepHours = () =>
     getSleepHours('Monday') +
@@ -34,3 +34,31 @@ const getActualSleepHours = () =>
     getSleepHours('Friday') +
     getSleepHours('Saturday') +
     getSleepHours('Sunday');
+
+
+
+const getIdealSleepHours = () => {
+    const idealHours = 8;
+    return idealHours * 7;
+};
+
+
+const calculateSleepDebt = () => {
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = getIdealSleepHours();
+
+    if (actualSleepHours === idealSleepHours) {
+        console.log('User got ' + (idealSleepHours) + '\'hour(s) sleep which is perfect amount of sleep! 😴😍');
+    }
+    else if (actualSleepHours > idealSleepHours) {
+        console.log('User got ' + (idealSleepHours) + '\'hour(s) more sleep which is more sleep than usual. SLEEP LESS 😠😒');
+    }
+    else if (idealSleepHours > actualSleepHours) {
+        console.log('You got ' + (idealSleepHours - actualSleepHours) + '\'hour(s) less sleep than you needed this week. Get some rest. 😪🥱');
+    }
+    else {
+        console.log('Something went Wrong!')
+    }
+};
+
+calculateSleepDebt()
