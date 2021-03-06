112f5ac5399eff9b9cfdc1423e0d1bea46f08251
shivam <shivamchouhan728@gmail.com>
2021-03-06 20:01:09 +0530

    :G: initial commit Your weekly sleep ours

---
+++ sleepDebtCalculator/scripts.js
@@ -0,0 +1,27 @@
+const getSleepHours = (day) => {
+    switch (day) {
+        case 'monday':
+            return 8
+            break;
+        case 'tuesday':
+            return 5
+            break;
+        case 'Wednesday':
+            return 3
+            break;
+        case 'thursday':
+            return 5
+            break;
+        case 'friday':
+            return 8
+            break;
+        case 'saturday':
+            return 4
+            break;
+        case 'sunday':
+            return 9
+            break;
+        default:
+            return "Error!"
+    }
+}
